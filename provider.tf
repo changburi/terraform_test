1e11000e3a1db0f5c2b731d9b79c168775244078
EC2 Default User <ec2-user@ip-172-31-42-190.ap-northeast-2.compute.internal>
2020-07-19 08:37:25 +0000

    initial commit

---
+++ provider.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+	region = "ap-northeast-2"
+}
+resource "aws_instance" "example" {
+	ami = "ami-0e92198843e11ccee"
+	instance_type = "t2.micro"
+
+	tags = {
+		Name = "terraform-example-01"
+	}
+}
